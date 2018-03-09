e332fd222602c0a757ff9190d1947503089c296c
DIR\s.kumar.vavilala <s.kumar.vavilala@accenture.com>
2018-03-09 18:52:26 +0530

    radio and checkboxes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {AppService} from '../../app/app.service';
+import { AppService } from '../../app/app.service';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  public message:string = 'santhosh'
+  public message: string = 'santhosh'
   public options = [];
+  optionText;
+  appService;
+  selectedTypeValue;
   constructor(public navCtrl: NavController, appService: AppService) {
     this.message = 'san'
     console.log(appService.msg);
-    appService.Options.push({id:1, value:'India'});
-    this.options = appService.Options;
+    this.appService = appService;
+    this.selectedTypeValue = 'default'
+  }
+
+  public addItems(optionText) {
+    if (optionText != '' && optionText != undefined) {
+      this.appService.Options.push({ id: 1, value: optionText });
+      this.options = this.appService.Options;
+      this.optionText = '';
+    }
   }
 
 }
